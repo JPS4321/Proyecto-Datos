0abecd1f5c7bd0e3b91b2efe032db67018ffe89e
JPS4321 <sol22102@uvg.edu.gt>
2023-03-13 20:25:07 -0600

    .

--- src/src/LispEnvironment.java
+++ src/src/LispEnvironment.java
@@ -1,2 +1,21 @@
+import java.util.*;
 public class LispEnvironment {
+
+    private final Map<String, LispExpression> symbolTable;
+
+    public LispEnvironment() {
+        symbolTable = new HashMap<>();
+    }
+
+    public void setSymbol(String symbol, LispExpression value) {
+        symbolTable.put(symbol, value);
+    }
+
+    public LispExpression getSymbol(String symbol) {
+        return symbolTable.get(symbol);
+    }
+
+    public boolean isSymbolDefined(String symbol) {
+        return symbolTable.containsKey(symbol);
+    }
 }

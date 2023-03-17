e4c81e233260ff47a060d596a93f967548bc2681
EstebanZG999 <zam22119@uvg.edu.gt>
2023-03-17 08:04:48 -0600

    // reader

--- src/src/Reader.java
+++ src/src/Reader.java
@@ -3,16 +3,17 @@ import java.util.*;
 public class Reader{
     ArrayList<String> lineas = new ArrayList<String>();
 
-    
+
     /** 
      * @param Arch
      */
+    //Reader lee la linea de codigo y se la transfiere al controlador
     public void Leer(String Arch){
         try (BufferedReader br = new BufferedReader(new FileReader(Arch))) {
             String lineaActual;
             while ((lineaActual = br.readLine()) != null) {
                 lineas.add(lineaActual);
-            }
+            }//Por si surge error leyendo el archivo, para que no salga error en el codigo
         } catch (IOException e) {
             System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
         }
